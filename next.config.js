22c47fe197520fb667ab80f2ea952ce944099adf
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-08-07 15:34:23 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,11 +13,18 @@ module.exports = {
         mangle: false,
     },
     optimizeCss: false,
+    presets: ['next/babel'],
     webpack: (config) => {
         config.module.rules.push({
             test: /\.css$/i,
             issuer: /\.[jt]sx?$/,
             use: ['style-loader', 'css-loader'],
+        });
+        config.optimization.minimizer.forEach((minimizer) => {
+            if (minimizer.options && minimizer.options.terserOptions) {
+                minimizer.options.terserOptions.compress = false;
+                minimizer.options.terserOptions.mangle = false;
+            }
         });
         return config;
     },
