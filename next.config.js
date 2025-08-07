312c89cbd14b87d24cf7eb5d41fa2fd08878da1f
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-08-07 15:11:40 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,4 +7,12 @@ module.exports = {
         locales: ['en', 'ru'],
         defaultLocale: "en",
     },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.css$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['style-loader', 'css-loader'],
+        });
+        return config;
+    },
 }
