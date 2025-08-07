f05d965a1618a880a4e7610b1eb38dff41bf0558
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-08-07 15:31:18 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ module.exports = {
         locales: ['en', 'ru'],
         defaultLocale: "en",
     },
+    swcMinify: false,
+    terserOptions: {
+        compress: false,
+        mangle: false,
+    },
+    optimizeCss: false,
     webpack: (config) => {
         config.module.rules.push({
             test: /\.css$/i,
