c355bf8bb6fc38f49b531d9538db3951fbf96022
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-08-08 14:20:08 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -37,14 +37,14 @@ class CustomDocument extends Document {
                     <title>Just an Ultimate Site Tool Demo</title>
                     <link rel="stylesheet" data-name="vs/editor/editor.main" href={`/third-party/monaco-editor/package/min/vs/editor/editor.main.css`} />
                     <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/base/worker/workerMain.js.map" as="fetch" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/html/htmlMode.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/html/htmlWorker.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/typescript/tsMode.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/typescript/tsWorker.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/css/cssMode.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/css/cssWorker.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/json/jsonMode.js" as="script" />
-                    <link rel="preload" href="/third-party/monaco-editor/package/min-maps/vs/language/json/jsonWorker.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/html/htmlMode.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/html/htmlWorker.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/typescript/tsMode.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/typescript/tsWorker.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/css/cssMode.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/css/cssWorker.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/json/jsonMode.js" as="script" />
+                    <link rel="preload" href="/third-party/monaco-editor/package/min/vs/language/json/jsonWorker.js" as="script" />
                 </Head>
                 <body>
                     <Main></Main>
